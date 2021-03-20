c30a6cee42e1032b97b655acd8ad23c571c94eae
BlazBone <bb6846@student.uni-lj.si>
2021-03-20 18:04:54 +0100

    dodelana 4 dn

--- dn/dn04/DN04b_63200063.c
+++ dn/dn04/DN04b_63200063.c
@@ -9,7 +9,6 @@ int *tabela;
 int main(void)
 {
     tabela = calloc(sizeof(int), MAKS_DOLZINA);
-
     int stStevil;
     int ciljnaVsota;
     scanf("%d %d",&stStevil, &ciljnaVsota);
@@ -20,23 +19,31 @@ int main(void)
         tabela[temp]++;
     }
     int sum = 0;
+    int soda = 1 + (ciljnaVsota % 2);
     for (int i = 0; i < ciljnaVsota/2 + 1; i++)
     {
         int druga = ciljnaVsota - i;
-        if (druga <= i )
+        if (druga < i )
         {
             break;
         }
-        
-        sum += tabela[i] * tabela[druga];
-
-
+        if (druga == i)
+        {
+            int koliko = tabela[druga];
+            int soda = koliko % 2;
+            if (soda)
+            {
+                sum += koliko/2 * (koliko/2 + 1);
+                //printf("smo tuki liha");
+            }else {
+                sum += koliko/2 * koliko/2;
+                //printf("smo tuki soda");
+            }
+            break;
+        }else {
+            sum += tabela[i] * tabela[druga];
+        }
     }
-    
     printf("%d\n",sum);
-    
-
-
-
     return 0;   
 }
